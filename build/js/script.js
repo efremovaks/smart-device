d0b1a3ce79d68c7303c3b16a2083d2eec97ecc79
kseniya <efremovaks@gmail.com>
2021-06-17 19:06:24 +0300

    футер есть, но аккордеон пока не закрывается

--- build/js/script.js
+++ build/js/script.js
@@ -9,8 +9,6 @@ var formName = modalForm.querySelector('input[name=Name]');
 var formTel = modalForm.querySelector('input[name=Phone]');
 var formText = modalForm.querySelector('textarea[name=Message]');
 
-console.log('sdsdsdsd')
-
 var isStorageSupport = true;
 var storage = '';
 
@@ -54,3 +52,42 @@ modalForm.addEventListener('submit', function (evt) {
     localStorage.setItem('login', formTel.value);
   }
 });
+
+
+// АККОРДЕОН
+
+var accordion = document.getElementById('accordion');
+var accordionItems = accordion.querySelectorAll('.accordion__item-header');
+var accordionContent = accordion.querySelectorAll('.accordion__item');
+
+
+for (var c = 0; c < accordionContent.length; c++) {
+  if (accordionContent[c].classList.contains('accordion_no-js')) {
+    accordionContent[c].classList.remove('accordion_no-js');
+  }
+}
+
+
+for (var i = 0; i < accordionItems.length; i++) {
+  accordionItems[i].addEventListener('click', function () {
+    accordionContent.forEach(element => {
+      element.classList.add('accordion__item-show');
+    });
+  });
+}
+
+// var activePanel;
+// accordionItems.forEach(function (item) {
+//   item.addEventListener('click', function () {
+//     for (var i = 0; i < accordionContent.length; i++) {
+//       accordionContent[i].classList.add('accordion__item-show');
+//       // accordionContent[i].nextElementSibling.classList.add('accordion__item-show');
+//       if (activePanel) {
+//         accordionContent[i].classList.remove('accordion__item-show');
+//         // activePanel.nextElementSibling.classList.remove('accordion__item-show');
+//       }
+//     }
+
+//     activePanel = (activePanel === accordionContent[i]) ? 0 : accordionContent[i];
+//   });
+// });
