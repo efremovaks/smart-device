f3cdecb45e9083e6f3d8b61627c4b58a83b0c9d7
kseniya <efremovaks@gmail.com>
2021-06-21 12:20:25 +0300

    маска на инпут телефона

--- build/js/script.js
+++ build/js/script.js
@@ -53,9 +53,48 @@ modalForm.addEventListener('submit', function (evt) {
   }
 });
 
-formTel.addEventListener('focus', function (evt) {
-  evt.target.formTel.textContent = '(+7'
-});
+
+// маска
+
+var input = document.querySelectorAll('input[name=Phone]');
+
+for (var i = 0; i < input.length; i++) {
+  input[i].addEventListener('focus', function () {
+    function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+      else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', pos);
+        range.moveStart('character', pos);
+        range.select()
+      }
+    }
+
+    function mask(event) {
+      var matrix = '+7(___)___-____',
+        i = 0,
+        def = matrix.replace(/\D/g, ''),
+        val = this.value.replace(/\D/g, '');
+      if (def.length >= val.length) val = def;
+      this.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a
+      });
+      if (event.type == 'blur') {
+        if (this.value.length == 2) this.value = ''
+      } else setCursorPosition(this.value.length, this)
+    };
+
+    for (var i = 0; i < input.length; i++) {
+      input[i].addEventListener('input', mask, false);
+      input[i].addEventListener('focus', mask, false);
+      input[i].addEventListener('blur', mask, false);
+    }
+  });
+}
+
+
 
 
 // АККОРДЕОН
